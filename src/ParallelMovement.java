0561b2f406bef41fab49470d95b6f9a7bb615b10
Grant Kurtz <th3guys2@gmail.com>
2013-02-09 21:45:57 -0500

    Completed the TestTraining class, which now picks out the five best stumps.

--- src/ParallelMovement.java
+++ src/ParallelMovement.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 
 /**
  * Author:      Grant Kurtz
+ *
+ * This Hypothesis assumes if our stock price closed higher than opened (went
+ * up), then the NASDAQ will do the same.
  */
 public class ParallelMovement implements Hypothesis{
 
@@ -15,6 +18,16 @@ public class ParallelMovement implements Hypothesis{
 		this.symbol = symbol;
 	}
 
+	/**
+	 * Returns the sign of the predicted parallel movement (both up, both down).
+	 *
+	 * @param ex	The example to test against.
+	 * @return		1 to mean that our stock price went up and the prediction is
+	 * 				that the NASDAQ will increase, while -1 indicates this stock
+	 * 				price fell, and the NASDAQ is expected to fall.  A 0
+	 * 				indicates missing data. A 0 suggests to the caller to ignore
+	 * 				the result, but not necessary to do so.
+	 */
 	public int prediction(Example ex){
 		if(data.get(ex.date) == null)
 			return 0;
--- src/Testtraining.java
+++ src/Testtraining.java
@@ -1,5 +1,6 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -11,11 +12,21 @@ public class TestTraining{
 	private static final String INPUT_FILE = "stumps.txt";
 	private static final String INPUT_PATH = INPUT_DIR + File.separator +
 											 INPUT_FILE;
+	private static final String OUTPUT_DIR = "stump_output";
+	private static final String OUTPUT_FILE = "best_stumps.txt";
+	private static final String OUTPUT_PATH = OUTPUT_DIR + File.separator +
+											  OUTPUT_FILE;
+	private static HashMap<String, HashMap<String, ModelData>> data = null;
+
+	private final int MAX_STUMPS = 5;
 
 	public static void main(String[] args){
 		System.out.println("Looking for input file '" + INPUT_PATH + "'...");
+		System.out.println("Looking for input file '" + OUTPUT_PATH + "'...");
 		Scanner input = null;
+		BufferedWriter output = null;
 		try{
+			output = new BufferedWriter(new FileWriter(OUTPUT_PATH));
 			input = new Scanner(new File(INPUT_PATH));
 		}
 		catch(FileNotFoundException e){
@@ -23,11 +34,86 @@ public class TestTraining{
 							   "Exiting...");
 			System.exit(1);
 		}
+		catch(IOException e){
+			System.err.println("Unable to open output file for processing! " +
+							   "Exiting...");
+			System.exit(1);
+		}
 		System.out.println("Found! Processing...");
-		new TestTraining(input);
+		try{
+			new TestTraining(input, output);
+		}
+		catch(IOException e){
+			System.err.println("Something happened while writing! Exiting...");
+			System.exit(1);
+		}
+		finally{
+			try{
+				output.close();
+			}
+			catch(IOException e){
+				System.err.println("Couldn't close the output file! Data " +
+								   "probably lost! Exiting...");
+				System.exit(1);
+			}
+		}
 	}
 
-	public TestTraining(Scanner input){
+	public TestTraining(Scanner input, BufferedWriter output)
+			throws IOException{
+		String line;
+		String[] values;
+		ArrayList<String> hypothesisData = new ArrayList<String>();
+		ArrayList<Double> hypothesisWeight = new ArrayList<Double>();
 
+		while(input.hasNext() && (line = input.nextLine()) != null){
+			values = line.split(",");
+
+			// All we need is the weight and hypothesis name, no need to do
+			// all the heavy processing of actually building the hypothesis.
+			// We also only care about the magnitude of the hypothesis, since a
+			// negative result just means that the hypothesis predicts the
+			// opposite.
+			double weight = Math.abs(Double.parseDouble(values[values.length - 1]));
+			insertIfBest(weight, line, hypothesisData, hypothesisWeight);
+		}
+
+		for(int i = 0; i < MAX_STUMPS; i++){
+			System.out.println(hypothesisData.get(i));
+			output.write(hypothesisData.get(i) + "\n");
+		}
+	}
+
+	/**
+	 * For simplicity, create a really lazy Insertion Sort routine.
+	 *
+	 * @param weight           The new weight to add.
+	 * @param line             The new Stringified hypothesis to add.
+	 * @param hypothesisData   The list of currently best hypothesis data.
+	 * @param hypothesisWeight The list of currently best hypothesis data weight
+	 *                         (used for sorting).
+	 */
+	private void insertIfBest(double weight, String line,
+							  ArrayList<String> hypothesisData,
+							  ArrayList<Double> hypothesisWeight){
+		int toBump = 0;
+		while(toBump < hypothesisWeight.size()){
+			if(hypothesisWeight.get(toBump) < weight){
+				break;
+			}
+			toBump++;
+		}
+
+		// We only want to store the top scoring hypothesis
+		if(toBump == MAX_STUMPS){
+			return;
+		}
+
+		hypothesisData.add(toBump, line);
+		hypothesisWeight.add(toBump, weight);
+		if(hypothesisData.size() > MAX_STUMPS){
+			hypothesisData.remove(MAX_STUMPS);
+			hypothesisWeight.remove(MAX_STUMPS);
+		}
 	}
 }
